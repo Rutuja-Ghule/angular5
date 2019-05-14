7007df4c7d095d81d677d223077c74b8080e2e49
roott2 <rutujaghule15@gmail.com>
2019-05-14 20:32:46 +0530

    changes in card component

--- src/app/cards-home/cards.component.ts
+++ src/app/cards-home/cards.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { CARD_DATA } from './cards.data';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
     selector: 'charts-home',
     templateUrl: 'cards.component.html',
     styleUrls: ['../app.component.css']
 })
-export class CardsHomeComponent {
+export class CardsHomeComponent implements OnInit {
+    ngOnInit() {
+        //console.log(CARD_DATA);
+    }
 
 }
---
+++ src/app/cards-home/cards.data.ts
@@ -0,0 +1,24 @@
+export const CARD_DATA = [
+    // {
+    //     title: 'Forms and testing',
+    //     list: ['Angular Forms', '- Template, reactive Forms, validations', '- Unit Testing using Jasmine'],
+    //     display: true
+    // }
+    {
+        title: 'Food for thought',
+        list: ['Foodie App - ', '- Searching', '- Filtering'],
+        display: true
+    }, {
+        title: 'Map your dream',
+        list: ['Playing with geograhical maps(in progress)', '- Tagging your favourite places', '- Searching locations and trip planning'],
+        display: false
+    }, {
+        title: 'Charts',
+        list: ['Playing with charts', '- Radial Progress', '- Liquid Fill Gauge'],
+        display: true
+    }, {
+        title: 'Let\'s Play',
+        list: ['Minesweeper in typescript'],
+        display: true
+    },
+]
